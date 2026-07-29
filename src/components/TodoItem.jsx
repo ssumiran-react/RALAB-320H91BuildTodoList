ce48699224321009e00914aedd867a18d137926a
ssumiran <ssumiran@yahoo.com>
2026-07-28 21:01:42 -0400

    Add Edit and Save hidden fields

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -4,27 +4,34 @@ export default function TodoItem(t) {
     const [isCompleted, setIsCompleted]= useState(t.completed);
     const [title, setTitle]= useState(t.title);
     const titleRef = useRef(null);
+    const [isEdit, setIsEdit]= useState(true);
 
     const onChangeCheckbox = () => {
       setIsCompleted(!isCompleted);
     };
 
-    const onChangeTitle = () => {
-      setTitle(title);
+    function onChangeTitle (e) {  //console.log ("title2 ",title);
+      e.preventDefault();
+      setTitle(titleRef.current.value);
+      onEdit(true)
+    };
+
+    function onEdit (val) {  
+      setIsEdit(val); //console.log ("isEdit ",isEdit);
     };
 
     return (
         <div style={{ paddingTop: "10px", paddingBottom: "10px" }} >
-            <div >
+            <div hidden={!isEdit} >
                 <input type="checkbox" checked={isCompleted ? true : false } 
                   onChange={onChangeCheckbox}></input>
                 <label> {title} </label>
-                <button>Edit</button>
+                <button onClick={()=>onEdit(false) }>Edit</button>
                 <button id="delTodo" disabled={!isCompleted ? true : false}>Delete</button>
             </div>
-            <div>
+            <div hidden={isEdit}>
                 <input ref={titleRef} type="text" defaultValue={title} ></input>
-                <button disabled={true}>Save</button>
+                <button disabled={false} onClick={onChangeTitle} >Save</button>
             </div>
             <hr></hr>
         </div>
